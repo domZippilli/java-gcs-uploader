8b44ec0a81bebf5b00aa0ee3d609915b756d2f67
Dom Zippilli <domz@google.com>
2019-12-18 09:23:44 +0000

    more slices

--- java-gcs-uploader/src/main/java/com/google/solutions/gcsuploader/Constants.java
+++ java-gcs-uploader/src/main/java/com/google/solutions/gcsuploader/Constants.java
@@ -2,8 +2,10 @@ package com.google.solutions.gcsuploader;
 
 public class Constants {
     public static final int CHUNK_SIZE = 15 * 1000 * 1000;
+
     public static final long SLICED_THRESHOLD = CHUNK_SIZE * 4;
-    public static final int MAX_SLICES = 10;
+    public static final int MAX_SLICES = 32; // No more than 32 due to simple implementation.
+
     public static final int SIMULTANEOUS_FILES = Runtime.getRuntime().availableProcessors();
     public static final int UPLOAD_THREADS = Runtime.getRuntime().availableProcessors() * 4;
 }
